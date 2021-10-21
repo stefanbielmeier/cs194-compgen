b253a2292b50b17d0863f909d48d44136d4a3d2a
stefanbielmeier <stefanbielmeier@icloud.com>
2021-10-21 09:13:32 -0700

    add program

--- hw3_Regression_RFCs/p5.py
+++ hw3_Regression_RFCs/p5.py
@@ -5,3 +5,15 @@ Problem 5: Training a variant effect predictor
 Training a machine learning algorithm to predict the pathogenicity of missense.
 Model will be trained on variants from ClinVar, a publicly accessible database of human variants and their associated phenotypes.
 """
+
+import numpy as np
+from sklearn.svm import SVC
+
+
+def main():
+
+	# TODO: Implement problem 5
+	pass
+
+if __name__ == '__main__':
+	main()
